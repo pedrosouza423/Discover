2d730d10b30d41a5eda4454ccbfc9a48b0e18586
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2022-02-10 17:45:26 -0300

    Learn about arrow function and callback

--- Guia estelar Javascript/script.js
+++ Guia estelar Javascript/script.js
@@ -1,17 +1,22 @@
-//Arrow function
-const soma = (num1, num2) => {
-    return num1 + num2
-}
-// console.log(soma(7,3))
+// //Arrow function
+// const soma = (num1, num2) => {
+//     return num1 + num2
+// }
+// // console.log(soma(7,3))
 
- //Callback é a função como retorno de outra função
+//  //Callback é a função como retorno de outra função
  
- function sayMyName(name){
-     console.log(name)
- }
+//  function sayMyName(name){
+//      console.log(name)
+//  }
 
- sayMyName(
-     () => {
-         console.log("This is a callback function")
-     }
- )
+//  sayMyName(
+//      () => {
+//          console.log("This is a callback function")
+//      }
+//  )
+
+ //Manipulando dados
+let nome = "Pedro";
+console.log(nome.length)
+
