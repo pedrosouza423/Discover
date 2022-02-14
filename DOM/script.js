8d92ee01b73c4b37cd04bccf40c6fa95ecbe8194
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2022-02-14 14:00:02 -0300

    finished study about background

---
+++ DOM/script.js
@@ -0,0 +1,21 @@
+// const search = document.querySelector('#search');
+
+// search.value = "Pedro"
+
+const header = document.querySelector('header');
+
+console.log(header);
+
+header.style.backgroundColor = "rgba(0, 55, 255,)"
+
+
+
+
+// header.setAttribute('id','header');
+
+// const headerId = document.querySelector('#header')
+
+
+// // headerId.removeAttribute("id");
+// console.log(headerId)
+
